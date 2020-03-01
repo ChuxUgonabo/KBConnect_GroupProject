80f0b87900e31013888dd735f476cf963a4bd9c8
Ugonabo <ugonaboj@student.douglascollege.ca>
2020-03-01 13:56:38 -0800

    created and added an Alert class

---
+++ src/com/kbconnect/entity/Alert.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package com.kbconnect.entity;
+
+/**
+ * @author John Ugonabo
+ *
+ */
+public class Alert {
+	private int _id;
+	private String _description;
+	java.sql.Date _dateOfLastUpdate; // date of last update
+	java.sql.Date _dateCreated; // date created
+	
+	
+	/**
+	 * @return the _id
+	 */
+	public int get_id() {
+		return _id;
+	}
+	/**
+	 * @param _id the _id to set
+	 */
+	public void set_id(int _id) {
+		this._id = _id;
+	}
+	/**
+	 * @return the _description
+	 */
+	public String get_description() {
+		return _description;
+	}
+	/**
+	 * @param _description the _description to set
+	 */
+	public void set_description(String _description) {
+		this._description = _description;
+	}
+	/**
+	 * @return the _dateOfLastUpdate
+	 */
+	public java.sql.Date get_dateOfLastUpdate() {
+		return _dateOfLastUpdate;
+	}
+	/**
+	 * @param _dateOfLastUpdate the _dateOfLastUpdate to set
+	 */
+	public void set_dateOfLastUpdate(java.sql.Date _dateOfLastUpdate) {
+		this._dateOfLastUpdate = _dateOfLastUpdate;
+	}
+	/**
+	 * @return the _dateCreated
+	 */
+	public java.sql.Date get_dateCreated() {
+		return _dateCreated;
+	}
+	/**
+	 * @param _dateCreated the _dateCreated to set
+	 */
+	public void set_dateCreated(java.sql.Date _dateCreated) {
+		this._dateCreated = _dateCreated;
+	}
+
+
+	
+
+}
