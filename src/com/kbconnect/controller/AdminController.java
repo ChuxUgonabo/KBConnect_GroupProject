1b30831bd664e6a06bdaf44c73e651b377f15839
singhg93 <singhg93@student.douglascollege.ca>
2020-03-11 21:08:22 -0700

    added the functionality to delete a user and an admin by another admin

--- src/com/kbconnect/controller/AdminController.java
+++ src/com/kbconnect/controller/AdminController.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.kbconnect.boundary.AdminDAO;
 import com.kbconnect.entity.Admin;
@@ -15,7 +16,7 @@ import com.kbconnect.entity.Admin;
  */
 @WebServlet("/AdminController")
 public class AdminController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -25,20 +26,22 @@ public class AdminController extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 
         AdminDAO adoa = new AdminDAO();
+
 
         String action = request.getParameter("action");
 
@@ -61,15 +64,43 @@ public class AdminController extends HttpServlet {
                 newAdmin.set_email(email);
                 newAdmin.set_address(address);
                 newAdmin.set_DOB(DOB);
+                newAdmin.set_isAdmin(true);
 
                 adoa.createUser(newAdmin);
-                response.sendRedirect("adminLogin.jsp");
+                response.sendRedirect("adminProfile.jsp");
+                break;
+                
+            case "update":
+                String updatedFullname = request.getParameter("fullName");
+                String updatedPassword = request.getParameter("password");
+                String updatedEmail = request.getParameter("email");
+                String updatedAddress = request.getParameter("address");
+
+                Admin oldAdmin = new Admin();
+
+                oldAdmin.set_fullName(updatedFullname);
+                oldAdmin.storeHashedPassword(updatedPassword);
+                oldAdmin.set_email(updatedEmail);
+                oldAdmin.set_address(updatedAddress);
+
+                adoa.updateUser(oldAdmin);
+                response.sendRedirect("adminProfile.jsp");
+                break;
+
+            case "delete":
+                // instantiate object of User
+                Admin adminToDelete;
+                // call getUser to get current User by id
+                adminToDelete = adoa.getUser(request.getParameter("username"));
+                // remove the admin from the database
+                adoa.deleteUser(adminToDelete);
+                response.sendRedirect("allCommuters.jsp");
                 break;
 
             default:
                 response.sendRedirect("404.jsp");
                 break;
         }
-	}
+    }
 
 }
--- src/com/kbconnect/controller/UserDAOController.java
+++ src/com/kbconnect/controller/UserDAOController.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.kbconnect.boundary.AdminDAO;
 import com.kbconnect.boundary.ComuterDAO;
@@ -20,161 +21,166 @@ import com.kbconnect.entity.User;
  */
 @WebServlet("/UserDAOController")
 public class UserDAOController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
 
-	// instantiate DAO
-	ComuterDAO bdao = new ComuterDAO();
-	AdminDAO adao = new AdminDAO();
-	// initialize an array list of allUser
-	ArrayList<User> allUser = new ArrayList<User>();
+    // instantiate DAO
+    ComuterDAO bdao = new ComuterDAO();
+    AdminDAO adao = new AdminDAO();
+    // initialize an array list of allUser
+    ArrayList<User> allUser = new ArrayList<User>();
 
-	public UserDAOController() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    public UserDAOController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *		response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *		response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-		// get all users for compare with a new user if it exists
-		allUser = bdao.getAllUsers();
-		// sentinel for checking the action if it is successful
-		boolean process = false;
-		Pattern pattern = Pattern.compile("^[a-z0-9A-Z_-]{2,15}$");
+        HttpSession thisSession = request.getSession();
+        String loggedUser = String.valueOf(thisSession.getAttribute("username"));
+        
+        // get all users for compare with a new user if it exists
+        allUser = bdao.getAllUsers();
+        // sentinel for checking the action if it is successful
+        boolean process = false;
+        Pattern pattern = Pattern.compile("^[a-z0-9A-Z_-]{2,15}$");
 
-		// See what the form action was
-		switch (request.getParameter("action")) {
-		
+        // See what the form action was
+        switch (request.getParameter("action")) {
+        
 
-		case "create":
+        case "create":
 
-			String fullname = request.getParameter("fullName");
-			String username = request.getParameter("username");
-			String password = request.getParameter("password");
-			String email = request.getParameter("email");
-			String address = request.getParameter("address");
-			String DOB = request.getParameter("DOB");
-			String authority = request.getParameter("authority");
+            String fullname = request.getParameter("fullName");
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            String email = request.getParameter("email");
+            String address = request.getParameter("address");
+            String DOB = request.getParameter("DOB");
+            String authority = request.getParameter("authority");
 
-			//
-			// ^ # Start of the line
-			// [a-z0-9_-] # Match characters and symbols in the list, a-z, 0-9, underscore,
-			// hyphen
-			// {3,15} # Length at least 2 characters and maximum length of 15
-			// $ # End of the line
-			boolean exist = false;
-			boolean valid = (username != null) && pattern.matcher(username).matches();
-			if (valid) {
-				// compare with every user in the list by username
-				for (User u : allUser) {
-					if (u.get_username().equals(username)) {
-						exist = true;
-						break;
-					}
-				}
+            //
+            // ^ # Start of the line
+            // [a-z0-9_-] # Match characters and symbols in the list, a-z, 0-9, underscore,
+            // hyphen
+            // {3,15} # Length at least 2 characters and maximum length of 15
+            // $ # End of the line
+            boolean exist = false;
+            boolean valid = (username != null) && pattern.matcher(username).matches();
+            if (valid) {
+                // compare with every user in the list by username
+                for (User u : allUser) {
+                    if (u.get_username().equals(username)) {
+                        exist = true;
+                        break;
+                    }
+                }
 
-				if (exist) {
-					response.sendRedirect("registeUser.jsp?error=same");
+                if (exist) {
+                    response.sendRedirect("registeUser.jsp?error=same");
 
-				} else {
+                } else {
 
-					// instantiate object of User with the given parameters
-					User newUser = new User(fullname, username, password, email, address, DOB);
-					// add new one to database
-					process = bdao.createUser(newUser);
-					System.out.println("creating: " + process);
-					// back to login page
-					if (authority != null){
-						response.sendRedirect("allCommuters.jsp");
-					}
-					else {
-						response.sendRedirect("login.jsp");
-					}
-				}
+                    // instantiate object of User with the given parameters
+                    User newUser = new User(fullname, username, password, email, address, DOB);
+                    // add new one to database
+                    process = bdao.createUser(newUser);
+                    System.out.println("creating: " + process);
+                    // back to login page
+                    if (authority != null){
+                        response.sendRedirect("allCommuters.jsp");
+                    }
+                    else {
+                        response.sendRedirect("login.jsp");
+                    }
+                }
 
-			}
+            }
 
-			else {
+            else {
 
-				response.sendRedirect("registeUser.jsp?error=invalid");
-			}
+                response.sendRedirect("registeUser.jsp?error=invalid");
+            }
 
-			break;
+            break;
 
-		case "update":
-			// instantiate object of user
-			User editUser;
+        case "update":
+            // instantiate object of user
+            User editUser;
 
             int id = Integer.parseInt(request.getParameter("userId"));
-			// call getUser() to get current user by id
-			editUser = bdao.getUser(id);
+            // call getUser() to get current user by id
+            editUser = bdao.getUser(id);
 
-			// set attributes value to object
+            // set attributes value to object
             String editedFullName = request.getParameter("fullName");
             String editedEmail = request.getParameter("email");
             String editedAddress = request.getParameter("address");
-            String editedDob = request.getParameter("DOB");
+//            String editedDob = request.getParameter("DOB");
 //          String editUsername = request.getParameter("userName");
-			String editAuthority = request.getParameter("authority");
+            String editAuthority = request.getParameter("authority");
 
 
-//			editUser.set_username(editUsername);
-			editUser.set_email(editedEmail);
-			editUser.set_address(editedAddress);
-			editUser.set_DOB(editedDob);
-			editUser.set_fullName(editedFullName);
+//          editUser.set_username(editUsername);
+            editUser.set_email(editedEmail);
+            editUser.set_address(editedAddress);
+//            editUser.set_DOB(editedDob);
+            editUser.set_fullName(editedFullName);
 
-			// call update method to editing
-			process = bdao.updateUser(editUser);
-			System.out.println("Editing: " + process);
+            // call update method to editing
+            process = bdao.updateUser(editUser);
+            System.out.println("Editing: " + process);
 
             if (editAuthority != null){
-				response.sendRedirect("allCommuters.jsp");
-			}
-			else {
-	            response.sendRedirect("profile.jsp");
-			}
-			break;
+                response.sendRedirect("allCommuters.jsp");
+            }
+            else {
+                response.sendRedirect("profile.jsp");
+            }
+            break;
 
 
-		case "delete":
-			// instantiate object of User
-			User curr = new User();
-			// call getUser to get current User by id
-			curr = bdao.getUser(request.getParameter("username"));
-			// add new one to database
-			System.out.println("Deleting is coming");
-			bdao.deleteUser(curr);
+        case "delete":
+
+            // only admin can delete a user, a user cannot delete himself
+            if ( adao.getUser(loggedUser) == null ) {
+                response.sendRedirect("403.jsp?message=forbidden");
+                break;
+            }
+
+            // instantiate object of User
+            User curr;
+            // call getUser to get current User by id
+            curr = bdao.getUser(request.getParameter("userId"));
+            // add new one to database
+            bdao.deleteUser(curr);
             response.sendRedirect("allCommuters.jsp");
-			break;
+            break;
 
-		default:
-			System.out.println("there is no action to do");
-			break;
-		}
+        default:
+            System.out.println("there is no action to do");
+            break;
+        }
 
-		// back to login page
-//		response.sendRedirect("login.jsp");
-
-//		doGet(request, response);
-	}
+    }
 
 }
