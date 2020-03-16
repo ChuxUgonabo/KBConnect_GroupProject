88298e4072c8ee0a481c32f38e61557d19ec9379
Chux Ugonabo <ugonaboj@student.douglascollege.ca>
2020-03-16 00:04:17 -0700

    created and added JUnit tests for User Entity

---
+++ src/test/com/kbconnect/boundary/CommuterDAOTest.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package test.com.kbconnect.boundary;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.kbconnect.boundary.CommuterDAO;
+import com.kbconnect.entity.User;
+
+/**
+ * @author user
+ *
+ */
+class CommuterDAOTest {
+	User userToTest;
+	CommuterDAO testUserDAO;
+
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");		
+//	}
+    @BeforeEach
+	void setUp() throws Exception {
+		// initialize a new user
+		userToTest = new User();
+		userToTest.set_username("newTestMas");
+		
+		testUserDAO = new CommuterDAO();
+	}
+	@Test
+	void testCreateUser() {
+//		assertTrue(testUserDAO.createUser(userToTest) == true);
+		assertEquals(true, testUserDAO.createUser(userToTest));
+	}
+	
+	// for some reason this test stopped working // would check it out tomorrow
+	@Test
+	void testUpdateUser() {
+		User updateTest = testUserDAO.getUser("newTestMas");
+		
+		updateTest.set_fullName("Test Master");
+		updateTest.set_password("12345678");
+		updateTest.set_email("test@gmail.com");
+		updateTest.set_DOB("1999-02-12");
+		updateTest.set_address("New Westminster, BC");
+//		assertTrue(testUserDAO.updateUser(userToTest) == true);
+		assertEquals(true, testUserDAO.updateUser(updateTest));
+	}
+//	
+	@Test
+	void deleteUser() {
+		User deleteTest = testUserDAO.getUser("newTestMas");
+		testUserDAO.deleteUser(deleteTest);
+//		assertTrue(testUserDAO.getUser(userToTest.get_id()) == null);
+		assertEquals(null, testUserDAO.getUser(deleteTest.get_username()));
+	}
+
+}
---
+++ src/test/com/kbconnect/entity/UserTest.java
@@ -0,0 +1,101 @@
+package test.com.kbconnect.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.kbconnect.entity.*;
+import com.kbconnect.boundary.*;
+
+class UserTest {
+	User userToTest;
+
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//
+//	}
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		// initialize a new user
+		userToTest = new User();
+
+		userToTest.set_id(1);
+		userToTest.set_fullName("Test Master");
+		userToTest.set_username("testM");
+ 		userToTest.set_password("12345678");
+		userToTest.set_email("test@gmail.com");
+		userToTest.set_DOB("1999-02-12");
+		userToTest.set_address("New Westminster, BC");
+	}
+
+	@Test
+	void testGetUsername() {
+		assertEquals("testM", userToTest.get_username());
+	}
+
+	@Test
+	void testGetPassword() {
+		assertEquals("12345678", userToTest.get_password());
+	}
+
+	@Test
+	void testGetFullName() {
+		assertEquals("Test Master", userToTest.get_fullName());
+	}
+
+	@Test
+	void testGetId() {
+		assertEquals(1, userToTest.get_id());
+	}
+
+	@Test
+	void testGetEmail() {
+		assertEquals("test@gmail.com", userToTest.get_email());
+	}
+
+	@Test
+	void testGetAddress() {
+		assertEquals("New Westminster, BC", userToTest.get_address());
+	}
+	
+	// testSets
+	@Test
+	void testSetUsername() {
+		userToTest.set_username("newTestM");
+		assertEquals("newTestM", userToTest.get_username());
+	}
+
+	@Test
+	void testSetPassword() {
+		userToTest.set_password("11111111");
+		assertEquals("11111111", userToTest.get_password());
+	}
+
+	@Test
+	void testSetFullName() {
+		userToTest.set_fullName("New Test Master");
+		assertEquals("New Test Master", userToTest.get_fullName());
+	}
+
+	@Test
+	void testSetId() {
+		userToTest.set_id(10);
+		assertEquals(10, userToTest.get_id());
+	}
+
+	@Test
+	void testSetEmail() {
+		userToTest.set_email("newTest@gmail.com");
+		assertEquals("newTest@gmail.com", userToTest.get_email());
+	}
+
+	@Test
+	void testSetAddress() {
+		userToTest.set_address("Burnaby, BC");
+		assertEquals("Burnaby, BC", userToTest.get_address());
+	}
+
+}
