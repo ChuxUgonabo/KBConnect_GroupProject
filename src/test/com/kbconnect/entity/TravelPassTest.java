e3f2119f7253064cfc0bb3dbd1a2fedf5bcc2788
300108357 <wsh5@student.douglascollege.ca>
2020-03-18 18:22:34 -0700

    added unit tests for TravelPass entity

---
+++ src/test/com/kbconnect/entity/TravelPassTest.java
@@ -0,0 +1,64 @@
+package test.com.kbconnect.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.kbconnect.entity.TravelPass;
+
+class TravelPassTest {
+
+	TravelPass travelPassTotest;
+
+	@BeforeEach
+	void setUp() throws Exception {
+        //instantiate a new travelpass for test
+		travelPassTotest = new TravelPass();
+		// set the value to attributes to test the getter method
+		travelPassTotest.set_passDuration("yearly");
+		travelPassTotest.set_price(200.00);
+		travelPassTotest.set_passType("normal");
+	}
+
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
+
+	@Test
+	void testGet_passDuration() {
+		assertEquals("yearly", travelPassTotest.get_passDuration());
+	}
+
+	@Test
+	void testSet_passDuration() {
+		travelPassTotest.set_passDuration("biweekly");
+		assertEquals("biweekly", travelPassTotest.get_passDuration());
+	}
+
+	@Test
+	void testGet_price() {
+		assertEquals(200.00, travelPassTotest.get_price());
+	}
+
+	@Test
+	public void testSet_price() {
+		travelPassTotest.set_price(100.00);
+		assertEquals(100.00, travelPassTotest.get_price());
+
+	}
+
+	@Test
+	void testGet_passType() {
+		assertEquals("normal", travelPassTotest.get_passType());
+	}
+
+	@Test
+	void testSet_passType() {
+		travelPassTotest.set_passType("private");
+		assertEquals("private", travelPassTotest.get_passType());
+
+	}
+
+}
