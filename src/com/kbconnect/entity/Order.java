3185d941ab39db4234ef71c3df67bf9d5192e3e2
singhg93 <singhg93@student.douglascollege.ca>
2020-03-22 23:37:17 -0700

    Added the user who placed the order and the admin who approves the order

--- src/com/kbconnect/entity/Order.java
+++ src/com/kbconnect/entity/Order.java
@@ -1,115 +1,155 @@
 
-
 package com.kbconnect.entity;
 
 import java.sql.Date;
 
 /**
  * Create an order object for commuter
+ * 
  * @author Weijun Shi
  *
  */
 public class Order {
 	private int _id;
 	private String _description;
+	private Product _productOrdered;
 	private double _unitPrice;
 	private int _quantity;
 	private double _amount;
 	private Date _transactionDate;
-	
-	/*reference database table 
-	 * mysql> desc orders;
-		+-----------------+--------------+------+-----+---------+----------------+
-		| Field           | Type         | Null | Key | Default | Extra          |
-		+-----------------+--------------+------+-----+---------+----------------+
-		| id              | int(11)      | NO   | PRI | NULL    | auto_increment |
-		| description     | varchar(255) | YES  |     | NULL    |                |
-		| quantity        | int(11)      | YES  |     | NULL    |                |
-		| unitPrice       | double(5,2)  | YES  |     | NULL    |                |
-		| amount          | double(6,2)  | YES  |     | NULL    |                |
-		| transactionDate | date         | YES  |     | NULL    |                |
-		+-----------------+--------------+------+-----+---------+----------------+
-		6 rows in set (0.00 sec)
+	private User _placedBy;
+	private Admin _approvedBy;
+
+	/*
+	 * reference database table mysql> desc orders;
+	 * +-----------------+--------------+------+-----+---------+----------------+ |
+	 * Field | Type | Null | Key | Default | Extra |
+	 * +-----------------+--------------+------+-----+---------+----------------+ |
+	 * id | int(11) | NO | PRI | NULL | auto_increment | | description |
+	 * varchar(255) | YES | | NULL | | | quantity | int(11) | YES | | NULL | | |
+	 * unitPrice | double(5,2) | YES | | NULL | | | amount | double(6,2) | YES | |
+	 * NULL | | | transactionDate | date | YES | | NULL | |
+	 * +-----------------+--------------+------+-----+---------+----------------+ 6
+	 * rows in set (0.00 sec)
 	 */
 
 	/**
-	 * default constructor 
+	 * default constructor
 	 */
 	public Order() {
 		super();
 	}
+
 	/**
 	 * @return the _id
 	 */
 	public int get_id() {
 		return _id;
 	}
+
 	/**
 	 * @param _id the _id to set
 	 */
 	public void set_id(int _id) {
 		this._id = _id;
 	}
+
 	/**
 	 * @return the _description
 	 */
 	public String get_description() {
 		return _description;
 	}
+
 	/**
 	 * @param _description the _description to set
 	 */
 	public void set_description(String _description) {
 		this._description = _description;
 	}
+
 	/**
 	 * @return the _unitPrice
 	 */
 	public double get_unitPrice() {
 		return _unitPrice;
 	}
+
 	/**
 	 * @param _unitPrice the _unitPrice to set
 	 */
 	public void set_unitPrice(double _unitPrice) {
 		this._unitPrice = _unitPrice;
 	}
+
 	/**
 	 * @return the _quantity
 	 */
 	public int get_quantity() {
 		return _quantity;
 	}
+
 	/**
 	 * @param _quantity the _quantity to set
 	 */
 	public void set_quantity(int _quantity) {
 		this._quantity = _quantity;
 	}
+
 	/**
 	 * @return the _amount
 	 */
 	public double get_amount() {
 		return _amount;
 	}
+
 	/**
 	 * @param _amount the _amount to set
 	 */
 	public void set_amount(double _amount) {
 		this._amount = _amount;
 	}
+
 	/**
 	 * @return the _transactionDate
 	 */
 	public Date get_transactionDate() {
 		return _transactionDate;
 	}
+
 	/**
 	 * @param _transactionDate the _transactionDate to set
 	 */
 	public void set_transactionDate(Date _transactionDate) {
 		this._transactionDate = _transactionDate;
 	}
-	
+
+	/**
+	 * @return the _placedBy
+	 */
+	public User get_placedBy() {
+		return _placedBy;
+	}
+
+	/**
+	 * @param _placedBy the _placedBy to set
+	 */
+	public void set_placedBy(User _placedBy) {
+		this._placedBy = _placedBy;
+	}
+
+	/**
+	 * @return the _approvedBy
+	 */
+	public Admin get_approvedBy() {
+		return _approvedBy;
+	}
+
+	/**
+	 * @param _approvedBy the _approvedBy to set
+	 */
+	public void set_approvedBy(Admin _approvedBy) {
+		this._approvedBy = _approvedBy;
+	}
 
 }
