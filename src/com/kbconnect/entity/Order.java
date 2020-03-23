df0ffc71a418ab00ec82e102cd9c0cae571a1330
300108357 <wsh5@student.douglascollege.ca>
2020-03-23 00:59:10 -0700

    changed the Order entity and OrderDAO.java

--- src/com/kbconnect/entity/Order.java
+++ src/com/kbconnect/entity/Order.java
@@ -11,27 +11,11 @@ import java.sql.Date;
  */
 public class Order {
 	private int _id;
-	private String _description;
-	private Product _productOrdered;
-	private double _unitPrice;
 	private int _quantity;
-	private double _amount;
 	private Date _transactionDate;
+	private Product _productOrdered;
 	private User _placedBy;
 	private Admin _approvedBy;
-
-	/*
-	 * reference database table mysql> desc orders;
-	 * +-----------------+--------------+------+-----+---------+----------------+ |
-	 * Field | Type | Null | Key | Default | Extra |
-	 * +-----------------+--------------+------+-----+---------+----------------+ |
-	 * id | int(11) | NO | PRI | NULL | auto_increment | | description |
-	 * varchar(255) | YES | | NULL | | | quantity | int(11) | YES | | NULL | | |
-	 * unitPrice | double(5,2) | YES | | NULL | | | amount | double(6,2) | YES | |
-	 * NULL | | | transactionDate | date | YES | | NULL | |
-	 * +-----------------+--------------+------+-----+---------+----------------+ 6
-	 * rows in set (0.00 sec)
-	 */
 
 	/**
 	 * default constructor
@@ -55,31 +39,17 @@ public class Order {
 	}
 
 	/**
-	 * @return the _description
+	 * @return the _productOrdered
 	 */
-	public String get_description() {
-		return _description;
+	public Product get_productOrdered() {
+		return _productOrdered;
 	}
 
 	/**
-	 * @param _description the _description to set
+	 * @param _productOrdered the _productOrdered to set
 	 */
-	public void set_description(String _description) {
-		this._description = _description;
-	}
-
-	/**
-	 * @return the _unitPrice
-	 */
-	public double get_unitPrice() {
-		return _unitPrice;
-	}
-
-	/**
-	 * @param _unitPrice the _unitPrice to set
-	 */
-	public void set_unitPrice(double _unitPrice) {
-		this._unitPrice = _unitPrice;
+	public void set_productOrdered(Product _productOrdered) {
+		this._productOrdered = _productOrdered;
 	}
 
 	/**
@@ -94,20 +64,6 @@ public class Order {
 	 */
 	public void set_quantity(int _quantity) {
 		this._quantity = _quantity;
-	}
-
-	/**
-	 * @return the _amount
-	 */
-	public double get_amount() {
-		return _amount;
-	}
-
-	/**
-	 * @param _amount the _amount to set
-	 */
-	public void set_amount(double _amount) {
-		this._amount = _amount;
 	}
 
 	/**
