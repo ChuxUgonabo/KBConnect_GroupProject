1760872a6cd6e9796105aa62f905de589828b794
300108357 <wsh5@student.douglascollege.ca>
2020-03-27 16:19:58 -0700

    modified the order.sql for new feature

--- sql/orders.sql
+++ sql/orders.sql
@@ -1,13 +1,14 @@
 use kbconnect;
-CREATE TABLE IF NOT EXISTS orders (
-    orderId int auto_increment,
+DROP TABLE IF EXISTS orders;
+CREATE TABLE  orders (
+    orderId int  not NULL auto_increment primary key,
     quantity int NOT NULL DEFAULT 1,
     transactionDate DATE,
     productId int,
     placedBy int,
     approvedBy int,
     approvalStatus BOOLEAN,
-    FOREIGN KEY productId REFERENCES products(id),
-    FOREIGN KEY placedBy REFERENCES user(id),
-    FOREIGN KEY approvedBy REFERENCES admin(id)
-)
+    FOREIGN KEY (productId) REFERENCES products(id),
+    FOREIGN KEY (placedBy) REFERENCES user(id),
+    FOREIGN KEY (approvedBy) REFERENCES admin(id)
+);
